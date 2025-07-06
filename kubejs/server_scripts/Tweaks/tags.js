4d4ff766debe747a6dfc1f16121e4504a4996bb2
Mitchell5200 <mitchell.louth@ozemail.com.au>
2025-07-06 10:58:37 +0800

    Fix enchanting, Closes #3

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -56,5 +56,9 @@ ServerEvents.tags('entity_type', allthemods => {
   ])
 })
 
+ServerEvents.tags('enchantment', allthemods =>{
+  allthemods.remove('minecraft:non_treasure', 'apothic_enchanting:miners_fervor')
+})
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10 LITE.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
